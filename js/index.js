0ba1aaf472268a3a3975914a3333a3a1cffdc2c8
unknown <s.140@hotmail.com>
2021-06-15 20:49:03 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.header-contenedor', {
+        duration: 2000,
+        origin : 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('#portada', {
+        duration: 2000,
+        origin : 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('#contenedor-info', {
+        duration: 4000,
+        origin : 'bottom',
+        distance: '400px'
+    });
